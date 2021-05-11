f439d049e4ea232d106368026998688f178e4862
João Nunes <joaocarlospaivanunes@outlook.pt>
2021-05-11 14:05:05 +0100

    ficha12

--- Ficha12/script.js
+++ Ficha12/script.js
@@ -8,14 +8,38 @@ var tabelaAno = document.getElementById("registosAno");
 function resultado(){
     var opcao = select.value;
     if(opcao == "RegistosConstantes"){
+        //var mes = prompt("Introduza um mês");
         tabelaConstante.style.display = "block";
         tabelaAno.style.display = "none";
+        fetch('Autenticacoes-Evolucao-por-Certificado.json')
+        .then(response => response.json())
+        .then(function(data){
+            data.forEach(function(autenticacoes){
+                    tabelaConstante.innerHTML += `<tr><td>${autenticacoes['Ano']}</td>
+                    <td>${autenticacoes['Mes']}</td><td>${autenticacoes['Chave-Movel-Digital']}</td>
+                    <td>${autenticacoes['Cartao-Cidadao']}</td>
+                    <td>${autenticacoes['Advogado']}</td>
+                    <td>${autenticacoes['Solicitador']}</td></tr>`
+            })
+        })
     }else if(opcao == "UtilizacoesAno"){
+        var ano = parseInt(prompt("Introduza um ano"));
+        console.log(ano);
         tabelaAno.style.display = "block";
         tabelaConstante.style.display = "none";
+        fetch('Autenticacoes-Evolucao-por-Certificado.json')
+        .then(response => response.json())
+        .then(function(data){
+            data.forEach(function(autenticacoes){
+                if(autenticacoes.Ano == ano){
+                    tabelaAno.innerHTML += `<tr><td>${autenticacoes['Ano']}</td>
+                    <td>${autenticacoes['Mes']}</td><td>${autenticacoes['Chave-Movel-Digital']}</td>
+                    <td>${autenticacoes['Cartao-Cidadao']}</td>
+                    <td>${autenticacoes['Advogado']}</td>
+                    <td>${autenticacoes['Solicitador']}</td></tr>`
+                }
+            })
+        })
     }
-    fetch('Autenticacoes-Evolucao-por-Certificado.json')
-    .then(response => response.json())
-    .then(data => console.log(data));
 
 }   
